135fceff7d80e42dbfa1261ff04ecf626fc941b5
Chris Riddle <stopimmediatley@gmail.com>
2023-09-08 09:37:21 -0400

    next back and start over buttons working, pushing before attempting next option

--- G-Slideshow/src/slideshow.jsx
+++ G-Slideshow/src/slideshow.jsx
@@ -4,23 +4,47 @@ import filmData from "./film-data.json"
 
 
 const Slideshow = ({ filmIndex, setFilmIndex }) => {
-const film = filmData[filmIndex]
+
+const [films, setFilms] = useState(filmData);
+
+const sortFilms = () => {
+  const sorted_films = [...films];
+  setFilms.sort(sorted_films)
+}
+
+const film = films[filmIndex]
+
+const handleNextClick = () => {
+  if (filmIndex < filmData.length - 1) {
+    setFilmIndex((prevIndex) => prevIndex + 1)
+  }
+}
+const handleBackClick = () => {
+  if (filmIndex > 0) {
+    setFilmIndex((prevIndex) => prevIndex - 1)
+  }
+}
+
+const handleStartOver = () => {
+  if (filmIndex != 0)
+    setFilmIndex(0)
+}
+
 
 
   return (
     <div>
-      {/* {filmData.map((film, index) => ( */}
-        {/* <div key={index} className="container"> */}
           <div className="fullBox">
             <h2>{film.title}</h2>
             <p>{film.original_title}</p>
             <img src={film.image} alt={film.title} />
             <p>Release Date: {film.release_date}</p>
             <p>Decription: {film.description}</p>
-            <button>NEXT</button>
+            <button onClick={sortFilms}>SORT BY RELESE DATE</button>
+            <button onClick={handleBackClick}>BACK</button>
+            <button onClick={handleStartOver}>START OVER</button>
+            <button onClick={handleNextClick}>NEXT</button>
           </div>
-        {/* </div> */}
-      {/* ))} */}
     </div>
   );
 }
